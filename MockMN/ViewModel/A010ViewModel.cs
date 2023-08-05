5594689eafef7ee518367439286faf0e12ff0947
本山大輝 <daiki120902@icloud.com>
2023-08-05 21:10:05 +0900

    CommunityToolkit.Mvvmをインストール。

--- MockMN/ViewModel/A010ViewModel.cs
+++ MockMN/ViewModel/A010ViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-namespace MockMN.ViewModel
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Collections;
+
+namespace MockMN.ViewModel;
+
+public class A010ViewModel : ObservableObject
 {
-	public class A010ViewModel
+	public A010ViewModel()
 	{
-		public A010ViewModel()
-		{
-		}
+			
 	}
 }
-
